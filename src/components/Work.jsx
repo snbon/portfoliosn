de35cc9d1733bb9084762e44db34ee585f000e7a
snbon <sweaniz@icloud.com>
2025-07-11 16:52:20 +0200

    add shiftend project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import WorkImg from '../assets/qrcodework.png';
 import ToDoList from '../assets/todolist.png';
-import BeyondNFT from '../assets/beyondnft.png'
+import BeyondNFT from '../assets/beyondnft.png';
+import ShiftEnd from '../assets/ShiftEnd.png';
+
 const Work = () => {
   return (
     <div name='work' className='w-full h-full sm:h-screen text-gray-300 bg-[#0a192f]'>
@@ -11,13 +13,32 @@ const Work = () => {
             Work
           </p>
           <p className='py-6'> Check out some of my recent work</p>
-          <p> !Expo projects are depricated - New Full Stack project uploading very soon! </p>    
+          <p> !Expo projects are depricated! </p>    
         </div>
 
 {/* Container */}
         <div className='grid grid-cols-1 gap-4'>
 
             {/* Grid Item */}
+            <div
+            style={{ backgroundImage: `url(${ShiftEnd}) ` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 duration-300'>
+              <div className='text-xl font-bold text-white tracking-wider text-center align-middle p-3'>
+                ShiftEnd: Het digitale dagontvangstboek. <br/> The software will be released after the approval of FOD Financiën.
+              </div>
+              <div className='pt-8 text-center'>
+                <a href='https://shiftend.be/' target="_blank" rel='noreferrer' >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500'>
+                    Try me
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>      
+
           <div
             style={{ backgroundImage: `url(${WorkImg}) ` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
@@ -87,7 +108,8 @@ const Work = () => {
                 </a>
               </div>
             </div>
-          </div>           
+          </div>  
+              
         </div>
       </div>
     </div>
