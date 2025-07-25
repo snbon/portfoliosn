9356c88c81f9ce5b272cdf80eb89ddd30a4041a0
snbon <sweaniz@icloud.com>
2025-07-25 18:25:53 +0200

    typo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/378b4506-308a-45c6-b0db-595f8095f66c" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-yellow-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'> Interested in working together? Submit the form below or email at - sweaniz@icloud.com </p>
+                <p className='text-gray-300 py-4'> Interested in working together? Submit the form below or view the dock </p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Thou Name is a must' name='name' required='true'/>
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='tataki@gmail.com' name='email' required='true' />
